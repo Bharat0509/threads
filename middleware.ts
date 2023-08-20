c8ef78407520cb1b2de83b73d96a695c37f3ba38
bharat <bhartbhammar3336@gmail.com>
2023-08-20 10:04:03 +0530

    AuthMiddleWare configured for organization

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-    publicRoutes: ["/", "/api/webhook/clerk"],
-    ignoredRoutes: ["/api/webhook/clerk"],
+    publicRoutes: ["/", "/api/webhook/cleark"],
+    ignoredRoutes: ["/api/webhook/cleark"],
 });
 
 export const config = {
